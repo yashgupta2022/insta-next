23c034bc7861a4dd9589011e3c4be3b3eb1b062d
YASH GUPTA <yash2002.gupta@gmail.com>
2024-01-25 18:41:09 +0530

    signin error removed

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth"
-import authOptions from "./authOptions.js"
-
+import {authOptions} from "./authOptions.js"
 
 const handler = NextAuth(authOptions)
 export {handler as GET , handler as POST} 

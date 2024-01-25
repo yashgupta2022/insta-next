1956151c1538b308f7e5eeea911c8281a8b22640
YASH GUPTA <yash2002.gupta@gmail.com>
2024-01-25 18:33:09 +0530

    route error removed

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,25 +1,6 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import authOptions from "./authOptions.js"
 
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    })
-  ], 
-  pages : {
-    signIn :'/auth/signin',
-  }, 
-  callbacks:{
-    async session({session, token,user}) {
-      session.user.username = session.user.name.split(" ").join("").toLowerCase()
-      session.user.uid = token.sub
-      return session
-    },
-  }
-}
 
 const handler = NextAuth(authOptions)
 export {handler as GET , handler as POST} 
